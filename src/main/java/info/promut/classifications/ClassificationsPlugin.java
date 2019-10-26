c609c292d30880d17ff3b9f231279a3bea11249a
Reißig <malte.reissig@iass-potsdam.de>
2019-10-26 17:29:54 +0100

    Initial commit: Declared application model.

---
+++ src/main/java/info/promut/classifications/ClassificationsPlugin.java
@@ -0,0 +1,16 @@
+package info.promut.classifications;
+
+import java.util.logging.Logger;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import systems.dmx.core.osgi.PluginActivator;
+
+@Path("/classification-systems")
+@Consumes("application/json")
+public class ClassificationsPlugin extends PluginActivator  {
+
+    private Logger log = Logger.getLogger(getClass().getName());
+    
+    // TBD.
+
+}
