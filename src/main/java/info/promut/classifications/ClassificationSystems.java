dd7609962e598d4fd9c12681032e16bfa820e176
Reißig <malte.reissig@iass-potsdam.de>
2019-12-17 12:50:06 +0000

    Added Migration2 for WS assignemnts (#1)

--- src/main/java/info/promut/classifications/ClassificationSystems.java
+++ src/main/java/info/promut/classifications/ClassificationSystems.java
@@ -3,14 +3,23 @@ package info.promut.classifications;
 import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
+import systems.dmx.accesscontrol.AccessControlService;
 import systems.dmx.core.osgi.PluginActivator;
+import systems.dmx.core.service.Inject;
+import systems.dmx.workspaces.WorkspacesService;
 
 @Path("/classification-systems")
 @Consumes("application/json")
 public class ClassificationSystems extends PluginActivator  {
 
     private Logger log = Logger.getLogger(getClass().getName());
+    public static final String CLASSIFICATIONS_WS_NAME = "Classification Systems";
     
+    @Inject WorkspacesService ws; 
+    @Inject AccessControlService as; 
+
     // TBD.
+    // - Automatic Workspace Assignment Rules
+    // - Core Workspace Assignment Facilities
 
 }
---
+++ src/main/java/info/promut/classifications/migrations/Migration2.java
@@ -0,0 +1,42 @@
+package org.deepamehta.plugins.signup.migrations;
+
+import systems.dmx.core.Topic;
+import systems.dmx.core.service.Inject;
+import systems.dmx.core.service.Migration;
+import systems.dmx.accesscontrol.AccessControlService;
+import systems.dmx.workspaces.WorkspacesService;
+import java.util.logging.Logger;
+import systems.dmx.core.TopicType;
+import systems.dmx.core.model.SimpleValue;
+import systems.dmx.core.service.accesscontrol.SharingMode;
+
+public class Migration2 extends Migration {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+    static final String CLASSIFICATIONS_WS_NAME = "Classification Systems";
+    static final String ADMIN_USERNAME = "admin";
+
+    @Inject
+    private WorkspacesService workspaces;
+    @Inject AccessControlService as; 
+
+    @Override
+    public void run() {
+
+        // 0) Check for WS, Make Sure Custom Plugin Workspace Does Exist
+        Topic csWorkspace = dmx.getTopicByValue("dmx.workspaces.name", new SimpleValue(CLASSIFICATIONS_WS_NAME));
+        if (csWorkspace == null) {
+                csWorkspace = workspaces.createWorkspace(CLASSIFICATIONS_WS_NAME, "dmx.classifications.workspace", SharingMode.CONFIDENTIAL);
+                as.setWorkspaceOwner(csWorkspace, ADMIN_USERNAME);
+        }
+        // 1) Assoc Top Level Topic Types
+        TopicType classification = dmx.getTopicType("org.purl.classifications.classification");
+        TopicType classificationSystem = dmx.getTopicType("org.purl.classifications.classification_system");
+        TopicType category = dmx.getTopicType("org.purl.classifications.category");
+        workspaces.assignToWorkspace(classification, csWorkspace.getId());
+        workspaces.assignToWorkspace(classificationSystem, csWorkspace.getId());
+        workspaces.assignToWorkspace(category, csWorkspace.getId());
+
+    }
+
+}
